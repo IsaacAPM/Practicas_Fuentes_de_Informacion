22c8c6fe558d78078cc626bbd97eb1db81e4d3fc
Isaac Alejandro Pimentel Morales <isaacapimentelm@gmail.com>
2020-05-04 00:07:12 -0500

    mapper corregido

--- MapperI.py
+++ MapperI.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May  3 23:23:31 2020
-
-@author: ipimentem
-"""
-
 # Selecciona campos de archivo CSV votacion
 # Formato de cada linea es:
 # Hora, Genero, Distrito, Candidato
@@ -12,18 +5,20 @@ Created on Sun May  3 23:23:31 2020
 # Este filtro toma dos elementos de stdin y entrega en stdout
 # esos elementos como <key,value> separados por tab
 # 
-
-import sys
+      
+import sys 
 
 for Line in sys.stdin:
-	Data = Line.strip().split(",")
-    distri = {};
-	if len(Data) == 4:
-		hora, genero, distrito, candidato = Data
-        if distri.get(distrito) == None:
+    Data = Line.strip().split(",")
+    distri = {}
+    
+    if len(Data) == 4:
+        hora, genero, distrito, candidato = Data
+        
+        if distro.get(distrito) == None:
             print("{0}\t{1}".format(distrito,1))
-            distri[distrito] = 1
         else:
-            print("{0}\t{1}".format(distrito,0))
+            print("{0}\{1}".format(distrito,0))
+            
 
     
